f735cdfe60b7e3fdf99b9d2f3e32442560de40df
Karl Santa <ksanta@mac.com>
2014-05-04 15:07:12 +1000

    Optimisation of processing stage

--- monitorLogFile.R
+++ monitorLogFile.R
@@ -10,39 +10,38 @@ monitorLogFile <- function(filename) {
         transaction <- character(numberOfRecords)
         user <- character(numberOfRecords)
         
-        print("Processing file")
-        
-        for(lineIndex in seq_along(data[,1])) {
-                # line is of type data.frame
-                line <- data[lineIndex,]
-                
-                # transpose and convert line to a vector, ready for splitting
-                lineVector <- as.vector(t(line))
-                
-                # split each element in lineVector
-                keyValuePairList <- strsplit(lineVector, "=")
-                
-                collectValues = function(element) {
-                        key <- element[1]
-                        value <- element[2]
-                        if(is.na(key)) {
-                                # skip if missing value
-                        } else if(key == "Start Time") {
-                                startTime[lineIndex] <<- value
-                        } else if(key == "End Time") {
-                                endTime[lineIndex] <<- value
-                        } else if(key == "Session Id") {
-                                sessionId[lineIndex] <<- value
-                        } else if(key == "Servlet Path") {
-                                transaction[lineIndex] <<- value
-                        } else if(key == "User Id") {
-                                user[lineIndex] <<- value
-                        }
+        collectValues = function(element) {
+                key <- element[1]
+                value <- element[2]
+                if(is.na(key)) {
+                        # skip if missing value
+                } else if(key == "Start Time") {
+                        startTime[lineIndex] <<- value
+                } else if(key == "End Time") {
+                        endTime[lineIndex] <<- value
+                } else if(key == "Session Id") {
+                        sessionId[lineIndex] <<- value
+                } else if(key == "Servlet Path") {
+                        transaction[lineIndex] <<- value
+                } else if(key == "User Id") {
+                        user[lineIndex] <<- value
                 }
+        }
+        
+        print("Processing")
+        
+        for(lineIndex in 1:numberOfRecords) {
+                line <- c(data[lineIndex,1], data[lineIndex,2], data[lineIndex,3],
+                                data[lineIndex,4], data[lineIndex,5], data[lineIndex,6])
+                
+                # split returns a list of key/value paired vectors
+                keyValuePairList <- strsplit(line, "=")
                 
                 lapply(keyValuePairList, collectValues)
         }
 
+        print("Converting")
+        
         # convert times to time types
         startTime <- strptime(startTime, "%Y-%m-%d %H:%M:%OS")
         endTime <- strptime(endTime, "%Y-%m-%d %H:%M:%OS")
@@ -54,6 +53,8 @@ monitorLogFile <- function(filename) {
         # convert Transaction and User to factors
         transaction <- as.factor(transaction)
         user <- as.factor(user)
+        
+        print("Building data frame")
         
         result <- data.frame(Start.Time=startTime, End.Time=endTime,Session.Id=sessionId,Transaction=transaction,
                              User=user, Duration=dateDiffs, stringsAsFactors=FALSE)
