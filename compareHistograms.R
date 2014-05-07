b13ba25fb7546e4e79e7015055f02c889aed7464
Karl Santa <ksanta@mac.com>
2014-05-07 21:59:40 +1000

    Create graphs in a subdirectory

--- compareHistograms.R
+++ compareHistograms.R
@@ -1,12 +1,17 @@
 # Loads 2 monitor log files and generates a histogram for each transaction type
-compareHistorgrams <- function(file1, file2) {
+compareHistograms <- function(file1, file2) {
         source("monitorLogFile.R")
         data1 <- monitorLogFile(file1)
         
-        for(transaction in levels(data1$Transaction)) {
-                print(transaction)
-                png(filename=paste(transaction, ".png", sep=""))
-                hist(data1$Duration[data1$Transaction == transaction])
+        if(!file.exists("histograms")) {
+                dir.create("histograms")
+        }
+        
+        for(index in seq_along(levels(data1$Transaction))) {
+                transaction <- levels(data1$Transaction)[index]
+                print(paste(index, "=", transaction))
+                png(filename=paste("histograms/", index, ".png", sep=""))
+                hist(data1$Duration[data1$Transaction == transaction], breaks=100)
                 dev.off()
         }
 }
