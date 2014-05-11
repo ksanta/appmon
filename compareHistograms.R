d127146ab70bc562c647a5ea956bbc155ea6e1dc
Karl Santa <ksanta@mac.com>
2014-05-12 08:03:19 +1000

    Change to density graphs, use lines, add legend. Delete existing graph files if found.

--- compareHistograms.R
+++ compareHistograms.R
@@ -3,18 +3,30 @@ compareHistograms <- function(file1, file2) {
         source("monitorLogFile.R")
         data1 <- monitorLogFile(file1)
         
-        if(!file.exists("histograms")) {
+        if(file.exists("histograms")) {
+                graphs <- list(list.files("histograms", full.names=TRUE))
+                do.call(file.remove,graphs)
+        } else {
                 dir.create("histograms")
         }
         
         transactions <- levels(data1$Transaction)
         for(index in seq_along(transactions)) {
+                # prep data
                 transaction <- transactions[index]
+                logDurations <- log(data1$Duration[data1$Transaction == transaction] * 1000)
+                if(length(logDurations) <= 2) {
+                        print(paste("*", index, "*", "=", transaction))
+                        next()
+                }
+                
+                # create graph
                 print(paste(index, "=", transaction))
                 png(filename=paste("histograms/", index, ".png", sep=""))
-                hist(data1$Duration[data1$Transaction == transaction], breaks=100, xlab="Duration (secs)",
-                     main=transaction)
-                
+                plot(density(logDurations), xlab="Duration (secs)", main=transaction, col="red", type="n")
+                lines(density(logDurations), col="red")
+                rug(logDurations)
+                legend("topright", lty=1, c("data1"), col=c("red"))
                 dev.off()
         }
 }
