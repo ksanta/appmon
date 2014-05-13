6a5861be8002f04050517ec0094c9aa44c2c6aec
Karl Santa <ksanta@users.noreply.github.com>
2014-05-13 17:32:20 +1000

    Fix where number of elements is defined by first line

--- monitorLogFile.R
+++ monitorLogFile.R
@@ -1,80 +1,81 @@
 ## Loads a monitor log file.  Attempts to load a .processed file first, to speed things up
 monitorLogFile <- function(filename) {
-        # Check if processed file already exists and try loading that - for speed
-        processedFileName <- paste(filename, ".processed", sep="")
-        if(file.exists(processedFileName)) {
-                print("Loading processed file")
-                
-                data <- read.csv(file=processedFileName, header=TRUE, colClasses=c("character", "POSIXct",
-                        "POSIXct", "character", "factor", "factor", "numeric"))
-                data$X <- NULL
-                return(data)
-        }
-        
-        print("Reading file")
-        data <- read.csv(file=filename, header=FALSE, stringsAsFactors=FALSE)
-        
-        numberOfRecords <- nrow(data)
-        startTime <- character(numberOfRecords)
-        endTime <- character(numberOfRecords)
-        sessionId <- character(numberOfRecords)
-        transaction <- character(numberOfRecords)
-        user <- character(numberOfRecords)
-        
-        collectValues = function(element) {
-                key <- element[1]
-                value <- element[2]
-                if(is.na(key)) {
-                        # skip if missing value
-                } else if(key == "Start Time") {
-                        startTime[lineIndex] <<- value
-                } else if(key == "End Time") {
-                        endTime[lineIndex] <<- value
-                } else if(key == "Session Id") {
-                        sessionId[lineIndex] <<- value
-                } else if(key == "Servlet Path") {
-                        transaction[lineIndex] <<- value
-                } else if(key == "User Id") {
-                        user[lineIndex] <<- value
-                }
-        }
-        
-        print("Processing")
-        
-        for(lineIndex in 1:numberOfRecords) {
-                # This is the most efficient way I could find to read a line from the data frame
-                line <- c(data[lineIndex,1], data[lineIndex,2], data[lineIndex,3],
-                                data[lineIndex,4], data[lineIndex,5], data[lineIndex,6])
-                
-                # split returns a LIST of key/value paired VECTORS
-                keyValuePairList <- strsplit(line, "=")
-                
-                lapply(keyValuePairList, collectValues)
-        }
-
-        print("Converting")
-        
-        # convert times to time types
-        startTime <- strptime(startTime, "%Y-%m-%d %H:%M:%OS")
-        endTime <- strptime(endTime, "%Y-%m-%d %H:%M:%OS")
-        
-        # calculate duration vector
-        dateDiffs <- difftime(endTime, startTime, units = "secs")
-        dateDiffs <- as.double(dateDiffs)
-        
-        # convert Transaction and User to factors
-        transaction <- as.factor(transaction)
-        user <- as.factor(user)
-        
-        print("Building data frame")
-        
-        result <- data.frame(Start.Time=startTime, End.Time=endTime,Session.Id=sessionId,Transaction=transaction,
-                             User=user, Duration=dateDiffs, stringsAsFactors=FALSE)
-
-        # Write to file so it's quicker to load next time
-        print("Saving processed monitor log file")
-        write.csv(result, file = processedFileName)
-        
-        # return
-        result
+  # Check if processed file already exists and try loading that - for speed
+  processedFileName <- paste(filename, ".processed", sep="")
+  if(file.exists(processedFileName)) {
+    print("Loading processed file")
+    
+    data <- read.csv(file=processedFileName, header=TRUE, colClasses=c("character", "POSIXct",
+                                                                       "POSIXct", "character", "factor", "factor", "numeric"))
+    data$X <- NULL
+    return(data)
+  }
+  
+  print("Reading file")
+  data <- read.csv(file=filename, header=FALSE, stringsAsFactors=FALSE, fill=TRUE,
+                   colClasses=c("character","character","character","character","character","character"))
+  
+  numberOfRecords <- nrow(data)
+  startTime <- character(numberOfRecords)
+  endTime <- character(numberOfRecords)
+  sessionId <- character(numberOfRecords)
+  transaction <- character(numberOfRecords)
+  user <- character(numberOfRecords)
+  
+  collectValues = function(element) {
+    key <- element[1]
+    value <- element[2]
+    if(is.na(key)) {
+      # skip if missing value
+    } else if(key == "Start Time") {
+      startTime[lineIndex] <<- value
+    } else if(key == "End Time") {
+      endTime[lineIndex] <<- value
+    } else if(key == "Session Id") {
+      sessionId[lineIndex] <<- value
+    } else if(key == "Servlet Path") {
+      transaction[lineIndex] <<- value
+    } else if(key == "User Id") {
+      user[lineIndex] <<- value
+    }
+  }
+  
+  print("Processing")
+  
+  for(lineIndex in 1:numberOfRecords) {
+    # This is the most efficient way I could find to read a line from the data frame
+    line <- c(data[lineIndex,1], data[lineIndex,2], data[lineIndex,3],
+              data[lineIndex,4], data[lineIndex,5], data[lineIndex,6])
+    
+    # split returns a LIST of key/value paired VECTORS
+    keyValuePairList <- strsplit(line, "=")
+    
+    lapply(keyValuePairList, collectValues)
+  }
+  
+  print("Converting")
+  
+  # convert times to time types
+  startTime <- strptime(startTime, "%Y-%m-%d %H:%M:%OS")
+  endTime <- strptime(endTime, "%Y-%m-%d %H:%M:%OS")
+  
+  # calculate duration vector
+  dateDiffs <- difftime(endTime, startTime, units = "secs")
+  dateDiffs <- as.double(dateDiffs)
+  
+  # convert Transaction and User to factors
+  transaction <- as.factor(transaction)
+  user <- as.factor(user)
+  
+  print("Building data frame")
+  
+  result <- data.frame(Start.Time=startTime, End.Time=endTime,Session.Id=sessionId,Transaction=transaction,
+                       User=user, Duration=dateDiffs, stringsAsFactors=FALSE)
+  
+  # Write to file so it's quicker to load next time
+  print("Saving processed monitor log file")
+  write.csv(result, file = processedFileName)
+  
+  # return
+  result
 }
