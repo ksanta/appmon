84b72c022d61d7eb5511617289e23c4621d0b089
Karl Santa <ksanta@mac.com>
2014-06-02 20:36:26 +1000

    Switch to ggplot2 graphing

--- compareHistograms.R
+++ compareHistograms.R
@@ -1,10 +1,17 @@
 # Loads 2 monitor log files and generates a histogram for each transaction type
 compareHistograms <- function(file1, file2, startHour = 0, endHour = 24) {
         source("monitorLogFile.R")
+        library(ggplot2)
         
         # Read in the 2 monitor log files
         data1 <- monitorLogFile(file1, startHour, endHour)
         data2 <- monitorLogFile(file2, startHour, endHour)
+
+        # TODO: Optimise using data.table features
+        data1$Filename <- file1
+        data2$Filename <- file2
+        data <- rbind(data1, data2)
+        setkey(x=data, Transaction)
         
         # Delete all existing graphs if they exist
         if(file.exists("histograms")) {
@@ -16,34 +23,16 @@ compareHistograms <- function(file1, file2, startHour = 0, endHour = 24) {
         
         transactionTypes <- levels(data1$Transaction)
         for(index in seq_along(transactionTypes)) {
-                # prep data, taking log10's of durations
                 transactionType <- transactionTypes[index]
-                logDurations1 <- data1[transactionType, log10(Duration)]$V1
-                logDurations2 <- data2[transactionType, log10(Duration)]$V1
                 
-                # skip if have less than 3 samples, as histogram won't work otherwise
-                if(length(logDurations1) < 3 || length(logDurations2) < 3) {
-                        print(paste(index, "= (SKIPPED) ", transactionType))
-                        next()
-                }
-                
-                hist1 <- hist(logDurations1, plot=FALSE)
-                hist2 <- hist(logDurations2, plot=FALSE)
-
-                maxCounts <- max(hist1$counts, hist2$counts)
-                
-                # open PNG graphics device
                 print(paste(index, "=", transactionType))
-                png(filename=paste("histograms/", index, ".png", sep=""), width=600, height=600)
                 
                 # build up the graph
-                plot(hist1, col=rgb(1,0,0,1/4), main=transactionType, xlab="Log Duration (millisecs)", 
-                     xlim=c(0,5), ylim=c(0, maxCounts), type="n")
-                lines(hist1, col=rgb(1,0,0,1/4))
-                lines(hist2, col=rgb(0,0,1,1/4))
-                legend("topright", lty=1, c(file1, file2), col=c("red", "blue"))
+                histogram <- geom_histogram(data=data[transactionType], mapping=aes(x=Duration, fill=Filename), binwidth=0.05)
+                labels <- labs(title=transactionType, y="Count")
                 
-                # TODO: Use try/catch to ensure device is closed
-                dev.off()
+                plot <- ggplot() + histogram + scale_x_log10() + labels
+                
+                ggsave(plot=plot, filename=paste("histograms/", index, ".png", sep=""))
         }
 }
--- usage.md
+++ usage.md
@@ -1,9 +1,15 @@
 Usage
-========================================================
+=====
 
 Pre-requisites
 --------------
 Download and install R, RStudio.
+Install some required packages.
+```
+install.packages("data.table")
+install.packages("ggplot2")
+```
+
 Download a copy of a few monitor log files.
 Use setwd() to set the working directory to where the R files are kept.
 
@@ -11,17 +17,29 @@ Generate graphs
 ---------------
 To generate probability density graphs for each transaction type:
 ```
-compareHistograms(firstFilename, secondFilename)
+compareHistograms(firstFilename, secondFilename, startHour, endHour)
 ```
-File names strings so they are quoted.
-compareHistograms() will create a "histograms" directory if not found and generate a PNG for each transaction type.
+* File names strings so they are quoted.
+* startHour and endHour are optional.  They should not be quoted.  They limit the transactions to just those between those hours.  The hours are given in 24 hour time.  For example: 15 = 3pm.
+* compareHistograms() will create a "histograms" directory if not found and generate a PNG for each transaction type.
+
+Compare volumes
+---------------
+To quickly compare the volumes between 2 monitor log files at the transaction level:
+```
+compareVolumes(firstFilename, secondFilename, startHour, endHour)
+```
+* File names strings so they are quoted.
+* startHour and endHour are optional.  They should not be quoted.  They limit the transactions to just those between those hours.  The hours are given in 24 hour time.  For example: 15 = 3pm.
+* The output will be given in the console window ranked by descending transaction counts.
 
 Loading monitor log files
 -------------------------
 To load a monitor log file into memory for adhoc analysis:
 ```
-monitorLogFile(filename)
+x <- monitorLogFile(filename)
 ```
+You're on your own for the analysis.
 
 Further work
 ------------
@@ -29,6 +47,7 @@ Would be great to be able to answer these questions:
 * What is the SessionId for a sample long running transaction type?
 * What does the grade of service look like over time of day for a transaction type?
   * Function takes file, transaction type, GoS percentage
-* Can we generate the histogram graphs for particular times in the day?
-* Can we compare transaction arrival rates per transaction type?
+* Can we plot transaction volumes over time with one line per transaction type?
 
+Bugs:
+* Legend is wrong in the compareHistogram() graphs
