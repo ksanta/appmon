376eeff326d6a17db0ca34c94d31e95a64751c58
Karl Santa <ksanta@users.noreply.github.com>
2014-09-24 11:59:20 +1000

    Greatly improve the plotting of graphs

--- gradeOfServiceOverTime.R
+++ gradeOfServiceOverTime.R
@@ -4,18 +4,19 @@
 gradeOfServiceOverTime <- function(file, quantile=0.95, startHour = 0, endHour = 24) {
   source("monitorLogFile.R")
   library(ggplot2)
+  library(scales)
   
   # Read in the monitor log file into a data table
   data <- monitorLogFile(file, startHour, endHour)
   
   # Create a per minute time sequence from start to end
-  startTime <- data[1]$Start.Time
-  endTime <- data[nrow(data)]$Start.Time
+  startTime <- min(data[,Start.Time])
+  endTime <- max(data[,Start.Time])
   timeBreaks <- seq.POSIXt(from=startTime, to=endTime, by="5 min")
-
+  
   # Create a vector of factors which will be used to "bin" each transaction
   groupingFactors <- cut(data$Start.Time, timeBreaks)
-
+  
   # Attach the grouping factors to the data table
   data[,grouping:=groupingFactors]
   
@@ -33,17 +34,30 @@ gradeOfServiceOverTime <- function(file, quantile=0.95, startHour = 0, endHour =
   for(index in seq_along(transactionTypes)) {
     transactionType <- transactionTypes[index]
     print(paste(index, "=", transactionType))
-  
-    # Create table of counts per time interval
-    groupedData <- data[transactionType, quantile(Duration, quantile), by=grouping]
-    setnames(groupedData, c("grouping", "V1"), c("Time", "Count"))
-  
-    ggp <- ggplot(data=groupedData, mapping=aes(x=Time, y=Count))
-    labels <- labs(title=transactionType, y="Duration", x="Time")
-    theme <- theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
-        plot.title = element_text(size = rel(0.75)))
     
-    plot <- ggp + labels + geom_point() + theme
+    # Create table of quantiles per time interval
+    groupedData <- data[transactionType, quantile(Duration, quantile), by=grouping]
+    setnames(groupedData, c("grouping", "V1"), c("Time", "Quantile"))
+    
+    # Convert the x-axis data series from factors to time, so they plot much better.
+    groupedData[,DateTime:=as.POSIXct(as.character(Time))]
+
+    # Exclude transaction types which don't have a minimum number of time samples
+    if(nrow(groupedData) < 5) {
+      print("Skipping this one because there are not enough time samples to plot")
+      next
+    }
+    
+    ggp <- ggplot(data=groupedData, mapping=aes(x=DateTime, y=Quantile))
+    
+    labels <- labs(title=transactionType, y=paste(quantile, "percentile (ms)"), x="Time")
+    
+    theme <- theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
+                   plot.title = element_text(size = rel(0.75)))
+    
+    timescale <- scale_x_datetime(breaks=date_breaks("1 hour"), minor_breaks=date_breaks("10 min"))
+    
+    plot <- ggp + geom_line(colour="blue") + labels + theme + expand_limits(y = 0) + timescale
     
     ggsave(plot=plot, filename=paste("quantiles/", index, ".png", sep=""))
   }
