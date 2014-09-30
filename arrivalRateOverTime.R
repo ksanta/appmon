8d4c77678462e4a558fa363ed0b342f9fa983c8e
Karl Santa <ksanta@users.noreply.github.com>
2014-09-30 14:52:10 +1000

    Add support for multiple monitor log files

--- arrivalRateOverTime.R
+++ arrivalRateOverTime.R
@@ -1,25 +1,42 @@
-# TODO: write a version which compares the transaction arrival rate per transaction
-# between 2 monitor log files
+# Creates a graph per transaction type showing arrival rate over time.
+# Given filename can be a regular expression so that it matches multiple files.
+# Default grouping of transaction counts is a 5 minute interval, though this can be changed with binPeriod
 
-arrivalRateOverTime <- function(file, startHour = 0, endHour = 24) {
-  source("monitorLogFile.R")
+arrivalRateOverTime <- function(file, startHour = 0, endHour = 24, binPeriod = "5 min", combineFiles = FALSE) {
+  source("multiMonitorLogFile.R")
   library(ggplot2)
   library(scales)
   
   # Read in the monitor log file into a data table
-  data <- monitorLogFile(file, startHour, endHour)
+  data <- multiMonitorLogFile(file, startHour, endHour)
+
+  # Flatten the filenames if we don't want to split by filenames
+  # Must flatten filenames BEFORE grouping
+  if(combineFiles == TRUE) {
+    data$Filename <- file
+  }
   
-  # Create a per minute time sequence from start to end
+  # Create a time sequence from start to end
   startTime <- min(data[,Start.Time])
   endTime <- max(data[,Start.Time])
-  timeBreaks <- seq.POSIXt(from=startTime, to=endTime, by="5 min")
+  timeBreaks <- seq.POSIXt(from=startTime, to=endTime, by=binPeriod)
   
   # Create a vector of factors which will be used to "bin" each transaction
-  groupingFactors <- cut(data$Start.Time, timeBreaks)
+  timeSlots <- cut(data$Start.Time, timeBreaks)
   
   # Attach the grouping factors to the data table
-  data[,grouping:=groupingFactors]
+  data[,Time:=timeSlots]
+
+  # Require keying of data table for grouping
+  setkey(data, Filename, Transaction, Time)
   
+  # Group counts by Filename, Transaction & TimeSlot
+  groupedData <- data[, length(Duration), by=list(Filename,Transaction,Time)]
+  setnames(groupedData, "V1", "Count")
+  
+  # Convert the x-axis data series from factors to time, so they plot much better.
+  groupedData[,DateTime:=as.POSIXct(as.character(Time))]
+
   # TODO: improve this by making it a function call
   # Delete all existing graphs if they exist
   if(file.exists("arrivalRates")) {
@@ -29,35 +46,33 @@ arrivalRateOverTime <- function(file, startHour = 0, endHour = 24) {
     dir.create("arrivalRates")
   }
   
-  transactionTypes <- levels(data$Transaction)
+  transactionTypes <- levels(groupedData$Transaction)
   
   for(index in seq_along(transactionTypes)) {
     transactionType <- transactionTypes[index]
     print(paste(index, "=", transactionType))
     
-    # Create table of counts per time interval
-    groupedData <- data[transactionType, length(Duration), by=grouping]
-    setnames(groupedData, c("grouping", "V1"), c("Time", "Count"))
-    
-    # Convert the x-axis data series from factors to time, so they plot much better.
-    groupedData[,DateTime:=as.POSIXct(as.character(Time))]
-    
+    # Subset only for the transaction type
+    graphData <- groupedData[Transaction == transactionType]
+        
     # Exclude transaction types which don't have a minimum number of time samples
-    if(nrow(groupedData) < 5) {
+    # This is a preference, not a technical limitation.  Otherwise can generate lots of empty graphs.
+    if(nrow(graphData) < 5) {
       print("Skipping this one because there are not enough time samples to plot")
       next
     }
     
-    ggp <- ggplot(data=groupedData, mapping=aes(x=DateTime, y=Count))
+    ggp <- ggplot(data=graphData, mapping=aes(x=DateTime, y=Count, colour=Filename))
     
     labels <- labs(title=transactionType, y="Count", x="Time")
     
     theme <- theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
-                   plot.title = element_text(size = rel(0.75)))
+                   plot.title = element_text(size = rel(0.75)), legend.position="bottom",
+                   legend.direction="vertical")
     
     timescale <- scale_x_datetime(breaks=date_breaks("1 hour"), minor_breaks=date_breaks("10 min"))
     
-    plot <- ggp + geom_line(colour="blue") + labels + theme + expand_limits(y = 0) + timescale
+    plot <- ggp + geom_line() + labels + theme + expand_limits(y = 0) + timescale
     
     ggsave(plot=plot, filename=paste("arrivalRates/", index, ".png", sep=""))
   }
