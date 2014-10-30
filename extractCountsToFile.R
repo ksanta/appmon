b4639bcf13d768a24ff9b129cad1f90a00caf83c
Karl Santa <ksanta@users.noreply.github.com>
2014-10-30 17:11:47 +1100

    Tidy up the exported file

--- extractCountsToFile.R
+++ extractCountsToFile.R
@@ -8,11 +8,11 @@ extractCountsToFile <- function(filename, startHour = 0, endHour = 24) {
   data <- multiMonitorLogFile(filename, startHour, endHour)
   
   # Extract the counts
-  summary.data <- data[,length(Duration),Transaction]
+  summary.data <- data[,list(Counts=length(Duration)),Transaction]
   
   # Sort by counts descending
-  summary.data <- arrange(summary.data, -V1)
+  summary.data <- arrange(summary.data, -Counts)
   
   # Save for easy import into Excel using Data -> From Text
-  write.table(summary.data, paste0(filename, "-counts.csv"))
+  write.table(summary.data, "Counts.csv", row.names = FALSE)
 }
