28a31077aa9bb155514d6555b1837a2b17f6c6fd
Karl Santa <ksanta@users.noreply.github.com>
2015-03-30 16:14:30 +1100

    Bring code up-to-date with all changes made to graphByTransaction

--- graphsByUser.R
+++ graphsByUser.R
@@ -1,8 +1,8 @@
 # Creates a set of graphs, grouped by user
 # Given filename can be a regular expression so that it matches multiple files.
-# Default grouping of transaction counts is a 5 minute interval, though this can be changed with binPeriod
+# Default grouping of transaction ArrivalRates is a 5 minute interval, though this can be changed with resolution
 
-graphsByUser <- function(file, startHour = 0, endHour = 24, quantile=0.95, binPeriod = "5 min", filterByTransaction = NULL) {
+graphsByUser <- function(file, startHour = 0, endHour = 24, resolution = "5 min", quantile1=0.95, quantile2=0.5, filterByTransaction = NULL, singleChart = FALSE) {
   source("multiMonitorLogFile.R")
   source("commonFunctions.R")
   library(ggplot2)
@@ -18,8 +18,14 @@ graphsByUser <- function(file, startHour = 0, endHour = 24, quantile=0.95, binPe
   # Optionally filter down to one transaction type
   if(!is.null(filterByTransaction)) {
     data <- data[data$Transaction == filterByTransaction]
-    data <- droplevels(data)
   }
+  
+  # Optionally flatten the users
+  if(singleChart == TRUE) {
+    data$User <- "All Users"
+  }
+
+  data <- droplevels(data)
   
   # Ignore the date portion - hardcode all to same value, will be hidden when graphing
   tempTimes <- as.POSIXlt(data$Start.Time)
@@ -35,18 +41,18 @@ graphsByUser <- function(file, startHour = 0, endHour = 24, quantile=0.95, binPe
   data$End.Time <- as.POSIXct(tempTimes)
   
   # Create factors which will be used to group each transaction into time intervals
-  timeSlots <- cut(data$Start.Time, binPeriod)
+  timeSlots <- cut(data$Start.Time, resolution)
   data[,Time:=timeSlots]
   
   # Require keying of data table for grouping
   setkey(data, Filename, User, Time)
   
-  # Group counts by Filename, User & TimeSlot
+  # Group ArrivalRates by Filename, User & TimeSlot
   # Quick refresher on data.table[i, j, by]:
   # i - selects the rows
   # j - calculated columns
   # by - grouping columns
-  groupedDataIncomplete <- data[, list(Count=length(Duration), GoS=quantile(Duration, quantile, na.rm=TRUE), Median=as.double(median(Duration, na.rm=TRUE))), by=list(Filename,User,Time)]
+  groupedDataIncomplete <- data[, list(ArrivalRate=length(Duration), quantile1=quantile(Duration, quantile1, na.rm=TRUE), quantile2=quantile(Duration, quantile2, na.rm=TRUE)), by=list(Filename,User,Time)]
   
   # Grouped data does not have values for all time samples, for example some low volume transactions 
   # do not have transactions coming in for every time period.
@@ -57,9 +63,9 @@ graphsByUser <- function(file, startHour = 0, endHour = 24, quantile=0.95, binPe
   groupedData <- merge(groupedDataIncomplete, allCombinationsTable, all=TRUE)
   
   # Set NA values to zero so they plot as a zero
-  groupedData$Count[is.na(groupedData$Count)] <- 0
-  groupedData$GoS[is.na(groupedData$GoS)] <- 0
-  groupedData$Median[is.na(groupedData$Median)] <- 0
+  groupedData$ArrivalRate[is.na(groupedData$ArrivalRate)] <- 0
+  groupedData$quantile1[is.na(groupedData$quantile1)] <- 0
+  groupedData$quantile2[is.na(groupedData$quantile2)] <- 0
 
   # Convert the x-axis data series from factors to time, so they plot much better.
   groupedData[,Time:=as.POSIXct(as.character(Time))]
@@ -67,31 +73,31 @@ graphsByUser <- function(file, startHour = 0, endHour = 24, quantile=0.95, binPe
   # Delete all existing graphs if they exist
   createOrEmptyDirectory(directory)
   
-  users <- levels(groupedData$User)
+  # Melt data for easier faceting when graphing
+  meltedData <- melt(groupedData, measure.vars = c("ArrivalRate", "quantile1", "quantile2"))
+
+  # Rename variable names so they are easier to understand
+  meltedData[variable == "quantile1"]$variable <- paste(quantile1, "Quantile")
+  meltedData[variable == "quantile2"]$variable <- paste(quantile2, "Quantile")
+  
+  users <- levels(meltedData$User)
   
   for(index in seq_along(users)) {
+    # Subsetting
     user <- users[index]
+    graphData <- meltedData[User == user]
+    
     print(paste(index, "=", user))
     
-    # Subset only for the user
-    graphData <- groupedData[User == user]
+    # Plotting
+    ggp <- ggplot(data=graphData, mapping=aes(x=Time, y=value, colour=Filename))
     
-    # Exclude data sets which don't have a minimum number of time samples
-    # This is a preference, not a technical limitation.  Otherwise can generate lots of empty graphs.
-    if(nrow(graphData) < 5) {
-      print("Skipping this one because there are not enough time samples to plot")
-      next
-    }
-    
-    # Melt the Count and GoS columns into variables for easier graphing
-    meltedData <- melt(graphData, measure.vars = c("Count", "GoS", "Median"))
-    
-    ggp <- ggplot(data=meltedData, mapping=aes(x=Time, y=value, colour=Filename))
-    
-    # Plot multiple graphs per image
     facets <- facet_grid(variable ~ ., scales="free")
     
-    labels <- labs(title=user, y="", x="Time")
+    title <- user
+    subtitle <- paste0("Resolution: ", resolution)
+    titleExpression <- bquote(atop(.(title), italic(.(subtitle))))
+    labels <- labs(title=titleExpression, y="", x="Time")
     
     theme <- theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), 
                    plot.title = element_text(size = rel(0.75)), legend.position="bottom",
@@ -99,7 +105,7 @@ graphsByUser <- function(file, startHour = 0, endHour = 24, quantile=0.95, binPe
     
     timescale <- scale_x_datetime(breaks=date_breaks("1 hour"), minor_breaks=date_breaks("10 min"), labels=date_format("%H:%M"))
     
-    plot <- ggp + facets + geom_line() + labels + theme + expand_limits(y = 0) + timescale
+    plot <- ggp + facets + geom_line(alpha=0.5) + labels + theme + expand_limits(y = 0) + timescale
     
     ggsave(plot=plot, filename=paste0(directory, "/", index, ".png"))
   }
